30efdc50b26e4006220b591c062fd016a2d5490e
radomichel <radomichel23@gmail.com>
2018-04-10 12:19:23 +0200

    Nowa funkcja odejmowanie

--- kalkulator.py
+++ kalkulator.py
@@ -5,3 +5,8 @@ y = int(input())
 def suma(x,y):
     wynik=x+y
     return wynik
+def iloczyn(x,y):
+    iloczyn=x*y
+    return iloczyn
+print('Suma tych liczb to: ', suma(x,y))
+print ('iloczyn tych liczb wynosi: ',iloczyn(x,y))
